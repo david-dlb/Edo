b59a25f827002d3d664597589a52beec8950750d
David Lezcano Becerra <davidbeceralezcano@gmail.com>
2023-06-19 23:32:59 -0400

    colab

--- Particular_case_1_point_3.py
+++
@@ -1,8 +0,0 @@
-import sympy as sp
-# where s is alpha and t is beta
-a, b, c, d, e, f, g, h, i, x, z, l, r, alpha, beta = sp.symbols('a b c d e f g h i x z l r alpha beta')
-
-target = alpha + g*((r - d)/a)**2 + ((r - d)/a)*c
-
-expanded = sp.expand(target)
-sp.pretty_print(expanded)
